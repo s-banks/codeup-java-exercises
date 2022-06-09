eaf968c3071902e498c3b13ac5508e5b92b21cc9
Shane Banks <shane.banks02@gmail.com>
2022-06-09 15:33:45 -0500

    first exercise done

---
+++ src/HelloWorld.java
@@ -0,0 +1,51 @@
+public class HelloWorld {
+    public static void main (String[] args) {
+/*
+ *  Multiline comments are back!!!
+ */
+//  System.out.printf("Hello, %s!", "Dennis");
+//    byte tinyNum = 127;
+//    int num = 235000;
+//   long id = 1L;
+//        System.out.println(num);
+//        System.out.println(tinyNum);
+//        System.out.println(id);
+//        System.out.println("Hello, Dennis" + "!");
+//    float floater = (float) 8.27;
+//    float noDecimal = 9;
+//        System.out.println(floater);
+//        System.out.println(noDecimal);
+//    char charmander = '&';
+//        System.out.println(charmander);
+//    String message = "\tI really really like marshmallows.";
+//        System.out.println(message);
+//int myFavoriteNumber = 5;
+    //    System.out.println(myFavoriteNumber);
+//String myString = "This is a string";
+//myString = 't';
+//myString = 3.14159;
+        //long myNumber = 123;
+        //float myNumber = 3.14;
+        //System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) "three";
+       // int x = 4;
+       // System.out.println(++x);
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+
+    }
+}
