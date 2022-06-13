8ddf3bb8623915482e6dab4e464ddb17463027ea
Shane Banks <shane.banks02@gmail.com>
2022-06-13 11:25:10 -0500

    strings exercise 2 complete

---
+++ src/Bob.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Do you want to play a game?");
+        String userInput = in.next();
+        if (userInput.endsWith("?")) {
+            System.out.println("Sure.");
+    } else if (userInput.endsWith("!")) {
+            System.out.println("Whoa, chill out!");
+        } else if (userInput.equals(" ")) {
+            System.out.println("Fine. Be that way!");
+        } else {
+            System.out.println("Whatever");
+        }
+    }
+}
