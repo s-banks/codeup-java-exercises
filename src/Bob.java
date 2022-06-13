afa6694e8dcbcad7622581e00d48cc8f20d92341
Shane Banks <shane.banks02@gmail.com>
2022-06-13 12:02:58 -0500

    exercise 2 corrections

--- src/Bob.java
+++ src/Bob.java
@@ -4,12 +4,12 @@ public class Bob {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Do you want to play a game?");
-        String userInput = in.next();
+        String userInput = in.nextLine();
         if (userInput.endsWith("?")) {
             System.out.println("Sure.");
-    } else if (userInput.endsWith("!")) {
+        } else if (userInput.endsWith("!")) {
             System.out.println("Whoa, chill out!");
-        } else if (userInput.equals(" ")) {
+        } else if (userInput.equals("")) {
             System.out.println("Fine. Be that way!");
         } else {
             System.out.println("Whatever");
