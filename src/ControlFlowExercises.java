341d075c55e6ec6d6a2b3189b81458087a48982a
Shane Banks <shane.banks02@gmail.com>
2022-06-13 10:37:37 -0500

    strings exercise 1 complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,5 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while (i <= 15) {
-            System.out.print(i + " ");
-            i++;
-        }
+
     }
 }
---
+++ src/StringExercise.java
@@ -0,0 +1,17 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String phrase = "We don't need no education\n" +
+                "We don't need no thought control";
+        System.out.println(phrase);
+
+        String phrase2 = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(phrase2);
+
+        String phrase3 = "In windows, the main drive is usually C:\\";
+        System.out.println(phrase3);
+
+        String phrase4 = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(phrase4);
+    }
+}
