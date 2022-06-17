55367e9d8886e3315dda847319e0b4e088439f74
Shane Banks <shane.banks02@gmail.com>
2022-06-17 15:08:00 -0500

    Finished earlier Input exercises

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,22 +1,57 @@
-//package util;
-//
-//import java.util.Scanner;
-//
-//public class Input {
-//	private Scanner scanner = new Scanner(System.in);
-//
-//	public String getString(){
-//		System.out.println("Enter a text string");
-//		return scanner.nextLine();
-//	}
+package util;
 
-//	public boolean yesNo() {
-//		System.out.println("Enter a boolean");
-//		 = String.valueOf(scanner.nextLine()).toLowerCase()
-//		return (val.equals("true") || val.equals("false"));
-//	}
-//	}
+import java.util.Scanner;
 
+public class Input {
+	private Scanner scanner = new Scanner(System.in);
+	
 
+	public String getString() {
+		System.out.println("Enter a string");
+		return scanner.nextLine();
+	}
 
-//}
+	public boolean yesNo() {
+		System.out.println("Please input: Yes or No");
+		String input = this.scanner.nextLine();
+		input = input.toLowerCase();
+		if (input.equals("yes")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public int getInt() {
+		System.out.println("Enter a whole number");
+		return scanner.nextInt();
+	}
+
+	public int getInt(int min, int max) {
+		System.out.printf("Enter a whole number between %d and %d.%n", min, max);
+		int userIn = scanner.nextInt();
+		if (userIn > min && userIn < max) {
+			return userIn;
+		} else {
+			System.err.println("Not a valid input");
+			return getInt(min, max);
+		}
+	}
+
+	public double getDouble() {
+		System.out.println("Enter a number");
+		return scanner.nextDouble();
+	}
+
+	public double getDouble(double min, double max) {
+		System.out.printf("Enter a number between %f and %f.%n", min, max);
+		double userIn = scanner.nextDouble();
+		if (userIn > min && userIn < max) {
+			return userIn;
+		} else {
+			System.err.println("Not a valid input");
+			return getDouble(min, max);
+		}
+	}
+
+}
---
+++ src/util/InputTest.java
@@ -0,0 +1,14 @@
+package util;
+
+public class InputTest {
+	public static void main(String[] args) {
+		Input in = new Input();
+
+		System.out.println(in.getString());
+		System.out.println(in.yesNo());
+		System.out.println(in.getInt());
+		System.out.println(in.getInt(2,8));
+		System.out.println(in.getDouble());
+		System.out.println(in.getDouble(2,8));
+	}
+}
