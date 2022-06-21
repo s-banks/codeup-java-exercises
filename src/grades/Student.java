284ed1f5c0477c7aefa8341fd72a5894e4ec9225
s-banks <shroud2@hotmail.com>
2022-06-21 18:08:13 -0500

    Student class done

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -4,27 +4,31 @@ import java.util.ArrayList;
 
 public class Student {
 	private String name;
-	private ArrayList<Integer> grade;
+	private ArrayList<Integer> grades;
 
 
-	public Student(String name, ArrayList<Integer> grade) {
+	public Student(String name, ArrayList<Integer> grades) {
 		this.name = name;
-		this.grade = grade;
+		this.grades = new ArrayList<>();
 	}
 
-	// returns the student's name
-	public String getName(); {
-
+// returns the student's name
+	public String getName() {
+		return this.name;
 	}
 
-	// adds the given grade to the grades property
+// adds the given grade to the grades property
 	public void addGrade(int grade) {
-
+		this.grades.add(grade);
 	}
 
-	// returns the average of the students grades
+//returns the average of the students grades
 	public double getGradeAverage() {
-
+		double gradeTotal = 0;
+		for ( int grade : this.grades) {
+			gradeTotal += grade;
+		}
+		return gradeTotal / this.grades.size();
 	}
 
 
