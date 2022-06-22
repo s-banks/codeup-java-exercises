ab2e15aad54cdde5f2d2fdd792ee97bf70d17543
Shane Banks <shane.banks02@gmail.com>
2022-06-22 14:19:32 -0500

    Loop basics part b complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,6 +2,10 @@
 //from Java I control statements
 public class ControlFlowExercises {
 	public static void main(String[] args) {
-
+		int i = 0;
+		do {
+			System.out.println(i);
+			i = i + 2;
+		} while (i <= 100);
 	}
 }
