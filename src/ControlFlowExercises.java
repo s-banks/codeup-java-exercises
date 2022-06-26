2558d95de8377aa4a2e971335f15e8aa684defb7
Shane Banks <shane.banks02@gmail.com>
2022-06-26 18:51:04 -0500

    3 table of powers complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,17 +1,25 @@
+import java.util.Scanner;
 
 //from Java I control statements
 public class ControlFlowExercises {
 	public static void main(String[] args) {
-		for (int i = 1; i < 101; i++) {
-			if (i % 3 == 0 && i % 5 == 0) {
-				System.out.println("FizzBuzz");
-			} else if (i % 5 == 0) {
-				System.out.println("Buzz");
-			} else if (i % 3 == 0) {
-				System.out.println("Fizz");
-			} else {
-				System.out.println(i);
-			}
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Building a numeric table of powers that starts with the number 1");
+		System.out.println("Please enter a number you would like to end with");
+		Integer num = Integer.valueOf(scanner.nextLine());
+		buildTable(num);
+	}
+
+
+	public static void buildTable(int num) {
+		System.out.println("Number  |  Squared  |  Cubed");
+		System.out.println("------  |  -------  |  -----");
+		for (int i = 1; i <= num; i++) {
+		System.out.println("  " + i + "     |     " + String.format("%.0f", Math.pow(i, 2)) + "     |    " + String.format("%.0f", Math.pow(i, 3)));
 		}
 	}
+
+
 }
+
+
