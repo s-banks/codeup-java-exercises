81bde9b63b20246b6f65f1d52dc66fbd395d6646
Shane Banks <shane.banks02@gmail.com>
2022-06-26 19:18:47 -0500

    Finished grade exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -4,22 +4,36 @@ import java.util.Scanner;
 public class ControlFlowExercises {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Building a numeric table of powers that starts with the number 1");
-		System.out.println("Please enter a number you would like to end with");
-		Integer num = Integer.valueOf(scanner.nextLine());
-		buildTable(num);
-	}
-
-
-	public static void buildTable(int num) {
-		System.out.println("Number  |  Squared  |  Cubed");
-		System.out.println("------  |  -------  |  -----");
-		for (int i = 1; i <= num; i++) {
-		System.out.println("  " + i + "     |     " + String.format("%.0f", Math.pow(i, 2)) + "     |    " + String.format("%.0f", Math.pow(i, 3)));
+		System.out.println("Would you like to convert a numeric grade to a letter grade?");
+		System.out.print("Please input y for yes or n for no: ");
+		String userInput = scanner.next();
+		while (userInput.equalsIgnoreCase("y")) {
+			System.out.print("What is the numeric grade? ");
+			int numGrade = scanner.nextInt();
+			if (numGrade >= 98) {
+				System.out.println("A+");
+			} else if (numGrade >=91) {
+				System.out.println("A");
+			} else if (numGrade >= 88) {
+				System.out.println("B+");
+			} else if (numGrade >=81) {
+				System.out.println("B");
+			} else if (numGrade >= 78) {
+				System.out.println("C+");
+			} else if (numGrade >=71) {
+				System.out.println("C");
+			}else if (numGrade >= 68) {
+				System.out.println("D+");
+			} else if (numGrade >=61) {
+				System.out.println("D");
+			} else {
+				System.out.println("F");
+			}
+			System.out.println("Would you like to input another grade?");
+			System.out.print("Please input y for yes or n for no: ");
+			userInput = scanner.next();
 		}
 	}
-
-
 }
 
 
