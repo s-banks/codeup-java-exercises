7969c36da1e12a45f90afd58dd51e64a7c065c35
s-banks <shroud2@hotmail.com>
2022-07-07 00:08:27 -0500

    Other exercises done

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,4 +1,48 @@
+import java.util.Scanner;
 
 //From Java I Methods
 public class HighLow {
+	static Scanner in = new Scanner(System.in);
+
+	public static int random() {
+		return (int) ((Math.random() * 100) + 1);
+	}
+
+	public static int valid () {
+		System.out.println("Enter a number between 1 and 100: ");
+		int num = in.nextInt();
+		if (num <= 100 && num >= 1){
+			return num;
+		} return valid();
+	}
+
+	public static void guess(int number, int answer, int guesses) {
+		if (guesses > 0) {
+			if (number == answer) {
+				System.out.println("Good guess!");
+			}
+			if (number > answer) {
+				System.out.println("Lower");
+				guess(valid(), answer, (guesses - 1));
+			}
+			if (number < answer) {
+				System.out.println("Higher");
+				guess(valid(), answer, (guesses - 1));
+			}
+		} else {
+			System.out.println("No guesses remaining.");
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Would you like to play a game? (Yes/No)");
+		String go = in.next();
+
+		if (go.equalsIgnoreCase("yes")) {
+			int number = random();
+
+			System.out.println("You have 10 chances to guess a number between 1 and 100");
+			guess(valid(),number,10);
+		}
+	}
 }
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -3,14 +3,13 @@ package util;
 public class InputTest {
 	public static void main(String[] args) {
 		Input in = new Input();
-
-//		System.out.println(in.getString());
-//		System.out.println(in.yesNo());
-//		System.out.println(in.getInt());
-//		System.out.println(in.getInt(2,8));
-//		System.out.println(in.getDouble());
-//		System.out.println(in.getDouble(2,8));
-//		System.out.println(in.getBinary());
-//		System.out.println(in.getHex());
+		System.out.println(in.getString());
+		System.out.println(in.yesNo());
+		System.out.println(in.getInt());
+		System.out.println(in.getInt(2,8));
+		System.out.println(in.getDouble());
+		System.out.println(in.getDouble(2,8));
+		System.out.println(in.getBinary());
+		System.out.println(in.getHex());
 	}
 }
